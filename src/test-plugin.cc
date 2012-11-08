50af43403c6b323bebf78e9207ea80c23b53ecd7
Fajran Iman Rusadi <fajran@gmail.com>
2012-11-08 20:37:22 +0100

    Add npapi-sdk's BasicPlugin Xcode project
    Copyright (c) 2008 Mozilla Corporation
    
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
    are met:
    
    - Redistributions of source code must retain the above copyright
    notice, this list of conditions and the following disclaimer.
    
    - Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
    
    - Neither the name of the Mozilla Corporation nor the names of its
    contributors may be used to endorse or promote products derived from
    this software without specific prior written permission.
    
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
    ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
    A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION
    OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

---
+++ src/test-plugin.cc
@@ -0,0 +1,9 @@
+//
+//  test-plugin.cc
+//  npTuioClient
+//
+//  Created by Fajran Iman Rusadi on 11/6/12.
+//
+//
+
+#include "test-plugin.h"
---
+++ src/test-plugin.h
@@ -0,0 +1,14 @@
+//
+//  test-plugin.h
+//  npTuioClient
+//
+//  Created by Fajran Iman Rusadi on 11/6/12.
+//
+//
+
+#ifndef __npTuioClient__test_plugin__
+#define __npTuioClient__test_plugin__
+
+#include <iostream>
+
+#endif /* defined(__npTuioClient__test_plugin__) */
