e85710e2f78e6e28d4e29fb9f6d1c6fe2290cf41
Fajran Iman Rusadi <fajran@gmail.com>
2012-11-14 05:24:31 +0100

    Print debugging messages only when the DBEUG flag is set

--- src/debug.h
+++ src/debug.h
@@ -3,12 +3,22 @@
 
 #include <stdio.h>
 
-#define D(format, args...) { \
+#ifdef DEBUG
+
+#define DF(format, args...) { \
   FILE* f = fopen("/tmp/npTuioClient.log", "a"); \
   fprintf(f, format "\n", ## args); \
   fflush(f); \
   fclose(f); \
 }
+
+#define D(format, args...) printf(format "\n", ## args);
+
+#else
+
+#define D(format, args...)
+
+#endif
 
 #endif
 
